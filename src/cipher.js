c9f4bde1c8d087c5cd3f624acf279593da07d2a7
adrianakatarina <dricakatarina@gmail.com>
2023-01-25 17:10:06 -0300

    terceiro commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,45 @@
 const cipher = {
-  // ...
+  encode,
+  decode
 };
 
 export default cipher;
+
+//Função Encriptar
+function encode(offset, string) {  
+  const senha = string.toUpperCase();
+  const chave = parseInt(offset);
+  //console.log(senha);
+  //console.log(typeof(chave));
+
+  let senhaString = "";
+  for (let i = 0; senha.length > i; i++) {    
+    //console.log("char", string.charCodeAt(i));
+    const tabela = (senha.charCodeAt(i) + 65 - chave) % 26 + 65;    
+    //console.log("calculo", tabela);        
+    const letra = String.fromCharCode(tabela);
+    //console.log("for", letra)    
+    senhaString += letra;
+  }
+  document.getElementById("mensagem").value = senhaString;  
+}
+
+//Função Decriptar
+
+function decode(offset, string) {
+  const senha = string.toUpperCase();
+  const chave = parseInt(offset);
+  //console.log(senha);
+  //console.log(typeof(chave));  
+  
+  let senhaString = "";
+  for (let i = 0; senha.length > i; i++) {
+    //console.log("char", string.charCodeAt(i));
+    const tabela = (senha.charCodeAt(i) - 65 + chave) % 26 + 65;
+    //console.log("calculo", tabela);
+    const letra = String.fromCharCode(tabela);
+    //console.log("for", letra)
+    senhaString += letra;
+  }
+  document.getElementById("mensagem").value = senhaString;
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
+
+//Botão Encriptar
+
+document.querySelector(".encriptar").addEventListener("click", function () {
+  const campo = document.querySelector("#mensagem").value;
+  const desloca = document.querySelector("#chave").value;
+  //console.log("d", typeof (desloca))    
+  cipher.encode(desloca,campo);  
+  //console.log("resultado", cipher.encode(chave, senha));
+});
+
+//Botão Decriptar
+
+document.querySelector(".decriptar").addEventListener("click", function () {
+  const campo = document.querySelector("#mensagem").value;
+  const desloca = document.querySelector("#chave").value;
+  //console.log("d", typeof (desloca));  
+  cipher.decode(desloca,campo);  
+  //console.log("resultado", cipher.decode(chave, senha));
+});
+
