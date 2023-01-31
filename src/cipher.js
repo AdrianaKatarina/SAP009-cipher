3cdd6f7628e7e64ba07d7d6a86cfa63794d240a3
adrianakatarina <dricakatarina@gmail.com>
2023-01-31 11:48:50 -0300

    quarto commit

--- src/cipher.js
+++ src/cipher.js
@@ -6,32 +6,38 @@ const cipher = {
 export default cipher;
 
 //Função Encriptar
-function encode(offset, string) {  
+function encode(offset, string) {
+  if (offset === "" || string === "" || typeof string !== "string" || string === [] || offset === null || offset === "0") {
+    throw new TypeError("Por favor, preencha todos os campos obrigatórios de acordo com as regras de funcionalidades");
+  }
+
+  let senhaString = "";
   const senha = string.toUpperCase();
   const chave = parseInt(offset);
   //console.log(senha);
-  //console.log(typeof(chave));
-  //let mensagem = document.getElementById("mensagem").value
-  let senhaString = "";
-  for (let i = 0; senha.length > i; i++) {    
+  //console.log(typeof(chave));  
+  for (let i = 0; senha.length > i; i++) {
     //console.log("char", string.charCodeAt(i));
-    const tabela = (senha.charCodeAt(i) - 65 + chave) % 26 + 65;    
-    //console.log("calculo", tabela);        
+    const tabela = (senha.charCodeAt(i) - 65 + chave) % 26 + 65;
+    //console.log("calculo", tabela);       
     const letra = String.fromCharCode(tabela);
     //console.log("for", letra)    
     senhaString += letra;
   }
-  return senhaString;  
+  return senhaString;
 }
 
 //Função Decriptar
 
 function decode(offset, string) {
+  if (offset === "" || string === "" || typeof string !== "string" || string === [] || offset === null || offset === "0") {
+    throw new TypeError("Por favor, preencha todos os campos obrigatórios de acordo com as regras de funcionalidades");
+  }
   const senha = string.toUpperCase();
   const chave = parseInt(offset);
   //console.log(senha);
   //console.log(typeof(chave));  
-  
+
   let senhaString = "";
   for (let i = 0; senha.length > i; i++) {
     //console.log("char", string.charCodeAt(i));
