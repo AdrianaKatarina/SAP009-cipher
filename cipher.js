9b725061b706a1a390a18ee69e73f05646f8b9df
adrianakatarina <dricakatarina@gmail.com>
2023-04-22 14:15:22 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -6,12 +6,14 @@ const cipher = {
 export default cipher;
 
 function encode(offset, string) {
-  if (offset === "" || string === "" || typeof string !== "string" || string === [] || offset === null || offset === "0") {
-    throw new TypeError(alert("Por favor, preencha todos os campos obrigatórios de acordo com as regras de funcionalidades"));
-  }
-
-  const chave = parseInt(offset);
-  if(chave >= 0 && chave < 100) {
+  try {
+    if (offset === "" || string === "" || typeof string !== "string" || string === [] || offset === null || offset === "0") {
+      throw new TypeError("Por favor, preencha todos os campos obrigatórios de acordo com as regras de funcionalidades.");
+    }
+    const chave = parseInt(offset);
+    if(chave < 1 || chave > 99){
+      throw new TypeError("Por favor, escolha deslocamento entre 1 e 99.");
+    }
     let senhaEncriptada = "";
     const senha = string.toUpperCase();
     const primeiraLetra = "A".charCodeAt(0);
@@ -22,18 +24,20 @@ function encode(offset, string) {
       senhaEncriptada += letra;
     }
     return senhaEncriptada;
-  } else{
-    throw new TypeError(alert("Por favor, escolha deslocamento entre 1 e 99"));
-  }  
+  }catch (e){
+    return e.message;
+  }
 }
 
 function decode(offset, string) {
-  if (offset === "" || string === "" || typeof string !== "string" || string === [] || offset === null || offset === "0") {
-    throw new TypeError(alert("Por favor, preencha todos os campos obrigatórios de acordo com as regras de funcionalidades"));
-  }
-
-  const chave = parseInt(offset);
-  if(chave >= 0 && chave < 100) {
+  try {
+    if (offset === "" || string === "" || typeof string !== "string" || string === [] || offset === null || offset === "0") {
+      throw new TypeError("Por favor, preencha todos os campos obrigatórios de acordo com as regras de funcionalidades.");
+    }
+    const chave = parseInt(offset);
+    if(chave < 1 || chave > 99){
+      throw new TypeError("Por favor, escolha deslocamento entre 1 e 99.");
+    }
     let senhaDecriptada = "";
     const senha = string.toUpperCase();
     const primeiraLetra = "A".charCodeAt(0);
@@ -44,7 +48,7 @@ function decode(offset, string) {
       senhaDecriptada += letra;
     }
     return senhaDecriptada;
-  } else{
-    throw new TypeError(alert("Por favor, escolha deslocamento entre 1 e 99"));
-  }  
+  }catch (e){
+    return e.message;
+  }
 }
